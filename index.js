467f715a79fd135efaa18f13fd6925f371d7ae11
Jeronimo Daniel Enciso-Canchola <62526638+encisodaniel@users.noreply.github.com>
2023-01-22 17:45:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function render() {
          let num = inputEl.value * 1;
     length.textContent = `${num} meters = ${(num * 3.281).toFixed(3)} feet | ${num} feet = ${(num / 3.281).toFixed(3)}` 
     
-    volume.textContent = `${num} liter = ${(num * 0.264).toFixed(3)} gallons| ${num} gallons = ${(num / 0.264).toFixed(3)}`
+    volume.textContent = `${num} liters = ${(num * 0.264).toFixed(3)} gallons| ${num} gallons = ${(num / 0.264).toFixed(3)}`
     
     mass.textContent =  `${num} kilos = ${(num * 2.204).toFixed(3)} pound | ${num} feet = ${(num / 2.204).toFixed(3)}`   
     
